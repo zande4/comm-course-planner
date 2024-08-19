e3717d2f02be2905867f525857d9974656094cc2
Zoe Anderson <122563457+zande4@users.noreply.github.com>
2024-08-19 15:11:05 -0400

    Add files via upload

--- courses.js
+++ courses.js
@@ -3,37 +3,37 @@ const C100 = new Course("100", "Communication and Social Process", false, false,
 
 let courseArray = [];
 
-  function parseCSV(callback) {
+function parseCSV(callback) {
     Papa.parse("courses.csv", {
-      download: true,
-      header: true,
-      dynamicTyping: true,
-      skipEmptyLines: true,
-      complete: function(results) {
-        courseArray = results.data.map(row => {
-          
-          return new Course(
-              String(row.number),
-              row.name,
-              row.starter,
-              row['COMM Exp'],
-              row['modes of inquiry'],
-              row.representation,
-              row.cel,
-              row.mapc,
-              row.mtpc,
-              row.ocw,
-              row.raa,
-              row.newMedia,
-              row.prereqs,
-              row.ideas,
-              row.description
-          );
-        });
-        if (callback) callback();
-      },
-      error: function(error) {
-        console.error("Parsing Error:", error);
-      }
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: function(results) {
+            courseArray = results.data.map(row => {
+        
+                return new Course(
+                    String(row.number),
+                    row.name,
+                    row.starter || false,
+                    row['COMM Exp'] || false,
+                    row['modes of inquiry'] || false,
+                    row.representation || false,
+                    row.cel || false,
+                    row.mapc || false,
+                    row.mtpc || false,
+                    row.ocw || false,
+                    row.raa || false,
+                    row.newMedia || false,
+                    row.prereqs || "NA",
+                    row.ideas || "NA",
+                    row.description
+                );
+            });
+            if (callback) callback();
+        },
+        error: function(error) {
+            console.error("Parsing Error:", error);
+        }
     });
-  }
+}
